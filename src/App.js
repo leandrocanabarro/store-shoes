fdfa76b91f745aae67862b3c9dc50e77248739a4
Leandro Canabarro <leandrocanabarro@gmail.com>
2020-02-13 17:11:47 -0300

    Add header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
+import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyle />
     </BrowserRouter>
